c9a4d01cb15db6ac8610996219bafa454fd62ee3
rabiyarumi <rabiyaarumi@gmail.com>
2025-05-05 20:06:39 +0600

    practice nullish operator

--- module_1/src/8_ternary_optional_nullish.ts
+++ module_1/src/8_ternary_optional_nullish.ts
@@ -11,6 +11,15 @@
 
   //========= Ternary operator
   const isAdult = age >= 18 ? "adult" : "not adult";
-  console.log({isAdult})
+  // console.log({isAdult})
 
+  //========== nullish coalescing operator ----> Decision making based on null or undefined ---> it will only work if the value is null or undefined
+
+  const isAuthenticated = null; //{ result1: Guest  }
+  // const isAuthenticated = undefined; //{ result1: Guest  }
+  // const isAuthenticated = ""; //{ result1: '' }
+  // const isAuthenticated = true; //{ result1: true }
+  // const isAuthenticated = false; //{ result1: false }
+  const result1 = isAuthenticated ?? "Guest"
+  console.log({result1})
 }
