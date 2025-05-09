5987e32e21e2d65ef29da849e75dc4d7f9880eeb
rabiyarumi <rabiyaarumi@gmail.com>
2025-05-09 22:30:17 +0600

    practice nullable type

---
+++ module_1/src/9_never_unknown_nullable.ts
@@ -0,0 +1,13 @@
+{
+     //============= nullable types / unknown types
+
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+  searchName(null);
+
+}
