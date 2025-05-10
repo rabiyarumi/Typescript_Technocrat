e76e03fe5cabca3f9ff74020de62246ccfc0929c
rabiyarumi <rabiyaarumi@gmail.com>
2025-05-10 22:16:45 +0600

    practice unknown type

--- module_1/src/9_never_unknown_nullable.ts
+++ module_1/src/9_never_unknown_nullable.ts
@@ -10,4 +10,20 @@
   };
   searchName(null);
 
+  //================== unknown type
+ const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else if (typeof value === "string") {
+      const [result, unit] = value.split(" ");
+      const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("wrong input");
+    }
+  };
+  getSpeedInMeterPerSecond(null);
+  
+
 }
