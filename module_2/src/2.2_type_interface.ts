e10727b8c796d2f477982a57f5628e67c95d6529
rabiyarumi <rabiyaarumi@gmail.com>
2025-05-22 23:25:02 +0600

    Practice type interface

--- module_2/src/2.2_type_interface.ts
+++ module_2/src/2.2_type_interface.ts
@@ -27,4 +27,39 @@ const user2 : User2={
 //---------- type alias can be used for primitive and non-primitive both type -------- but type interface can only be used for non-primitive type 
 type RollNumber = number;
 //
+
+
+//------ if we want to add a new property we can use "intersection " for "type alias" and we can use "extend" keyword for " type intersection" 
+
+//----- type alias
+type UserWithRole1 = User1 & {role: string}
+const userWithRole1: UserWithRole1={
+    name: "Rumi",
+    age: 21,
+    role: "Admin"
+}
+
+//---- type interface
+interface UserWithRole2 extends User2 {
+    role: string
+}
+
+const userWithRole2: UserWithRole2={
+    name: "Rumi",
+    age: 21,
+    role: "User"
+}
+
+
+//=============== type interface for array
+type Roll1 = number[];
+
+interface Roll2 {
+    [index: number]: number;
+}
+
+const rollNumber1: Roll1 = [ 4, 6, 8,7];
+const rollNumber2: Roll2 = [ 4, 6, 8,7];
+
+
 }
