d670edaa5f8568dc63434c2dfed2885e66c9de9f
rabiyarumi <rabiyaarumi@gmail.com>
2025-07-25 21:01:52 +0600

    practice generic type

---
+++ module_2/src/2.3_generics.ts
@@ -0,0 +1,29 @@
+{
+    // ============= Generic type
+
+    const rollNumbers1: number[] = [4, 7, 9,3,4]  // type alias
+    const rollNumbers2: Array<number> = [4, 7, 9,3,4]  // type Generics
+
+
+    // If we want to make it reusable 
+    type GenericBoolean = Array<boolean>
+    const isAdmin2 : GenericBoolean = [true, false, true] // type Generics
+
+    // if we want to make it dynamically reusable
+    type GenericArray<T> = Array<T>
+    const mentors2: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"] // type Generics
+
+    //================= array of object using generic type
+    const user: GenericArray<{ name: string, age: number}> = [
+        {
+            name: "Mezbah",
+            age: 100,
+        },
+        {
+            name: "Halim",
+            age: 200
+        }
+    ]
+
+}
+
