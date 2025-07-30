53534342977eab160777fe0488cecba836c0998a
rabiyarumi <rabiyaarumi@gmail.com>
2025-07-30 21:51:53 +0600

    practice generic with function

--- module_2/src/2.5_generic_with_function.ts
+++ module_2/src/2.5_generic_with_function.ts
@@ -13,7 +13,35 @@
         return [param]
     }
     const res2 = arrayWithGeneric<boolean>(true)
+    
     const resObj = arrayWithGeneric<{id: number; name: string}>({id: 334, name: "Mr. X"})
+
+
+
+    // function with generics and tuple
+    const arrayWithGenericTuple = <T, Q>(param1: T, param2: Q) : [T, Q] =>{
+        return [param1, param2]
+    }
+    const res3 = arrayWithGenericTuple<string, number>("Bangladesh", 444)
+    const res4 = arrayWithGenericTuple<string, {name: string}>("Bangladesh", {name: "Asia"})
+
+    const resObj3 = arrayWithGeneric<{id: number; name: string}>({id: 334, name: "Mr. X"})
+
+
+    
+    //function with generics
+    const addCourseToStudent = <T>(student: T) => {
+        const course = "Next Level Web Development"
+
+        return{
+            ...student,
+            course
+        }
+    }
+
+    const student1 = addCourseToStudent({name: "Mr. X", email: "x@gmail.com", devType: "Backend Developer"})
+
+    const student2 = addCourseToStudent({name: "Mr. Y", email: "y@gmail.com", experience: "2 Years"})
 
 
 
