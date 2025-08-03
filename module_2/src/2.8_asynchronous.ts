9b645a91909cd83de9ed55068771bafec57c7277
rabiyarumi <rabiyaarumi@gmail.com>
2025-08-03 13:33:43 +0600

    practice asynchronous ts

--- module_2/src/2.8_asynchronous.ts
+++ module_2/src/2.8_asynchronous.ts
@@ -1,6 +1,19 @@
 {
-
     //============== Promise
+
+    type Todo ={
+        id: number;
+        userId: number;
+        title: string;
+        completed: false;
+    }
+    const getTodo = async (): Promise<Todo> => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await res.json();
+        console.log(data)
+        return data
+    }
+    getTodo()
 
     //Simulate
     const createPromise =() : Promise<string> => {
@@ -18,7 +31,7 @@
     // calling create promise function
     const showData = async () : Promise<string> => {
         const data : string = await createPromise();
-        console.log(data);
+        // console.log(data);
         return data
     }
 
