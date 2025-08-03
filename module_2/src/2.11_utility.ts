ddf62b40af71e2d785f4c02c619b4263af9352ef
rabiyarumi <rabiyaarumi@gmail.com>
2025-08-03 22:06:31 +0600

    Practice readOnly & record type

--- module_2/src/2.11_utility.ts
+++ module_2/src/2.11_utility.ts
@@ -24,4 +24,28 @@
     // Partial ==> it will make every property optional 
     type PersonPartial = Partial<Person>
 
+    // ReadOnly ==> it can never be change
+    type PersonReadOnly = Readonly<Person>
+
+    const person1 : PersonReadOnly ={
+        name: "Mr X",
+        age: 22,
+        email: "x@gmail.com",
+        contactNo: "0173"
+    }
+
+    // Record ==> it will dynamically accept the key and value both
+    type MyObj = Record<string, string>
+
+    const obj1: MyObj={
+        a: "aa",
+        b: "bb",
+        c: "cc",
+        d: "dd"
+    }
+
+    // if we don't know the type of the value 
+    const EmptyObj : Record<string, unknown> = {}
+
+
 }
